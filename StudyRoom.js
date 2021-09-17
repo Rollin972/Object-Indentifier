198fc3349c1da1425d93355f826a01537362bdde
Rollin754 <75938189+Rollin972@users.noreply.github.com>
2021-09-17 19:49:02 +0530

    Add files via upload

---
+++ StudyRoom.js
@@ -0,0 +1,57 @@
+image2 = "";
+status = "";
+objects = [];
+
+
+function preload()
+{
+   image2 = loadImage("StudyRoom.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(800,600);
+    canvas.position(540,330);
+    dectector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("st").innerHTML = "Detecting Objects....";
+}
+
+function modelLoaded()
+{
+   console.log("model loaded successfully!");
+   status = true;
+   dectector.detect(image2,gotResult);
+}
+
+function gotResult(error,result)
+{
+   if(error)
+   {
+       console.log(error);
+   }
+   else
+   {
+       console.log(result);
+       objects = result;
+   }
+}
+
+function draw()
+{
+    image(image2,0,0,800,600);
+    if(status !="")
+    {
+        document.getElementById("st").innerHTML = "Objects Detected !";
+        document.getElementById("n").innerHTML = "Number Of Objects = "+objects.length;
+        for(i=0;i<objects.length;i++)
+        {
+            fill("black");
+            percentage = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15 );
+            noFill();
+            stroke("black");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+
---
+++ main.js
@@ -0,0 +1,35 @@
+function no1()
+{
+    window.location = "study_room.html";
+}
+
+function no2()
+{
+    window.location = "Dining_Table.html";
+}
+
+function no3()
+{
+    window.location = "Bed_Room.html";
+}
+
+function no4()
+{
+    window.location = "Balcony.html";
+}
+
+function no5()
+{
+    window.location = "Pets.html";
+}
+
+function no6()
+{
+    window.location = "Documents_And_Files.html";
+}
+
+function back()
+{
+    window.location = "index.html";
+}
+
